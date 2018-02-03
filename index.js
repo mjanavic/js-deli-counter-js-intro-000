d6b577e418597223130c12756fe1d345609f9298
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-03 14:42:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  for(var i = 0; i<katzDeliLine; i++) {
+  for(var i = 0; i<katzDeliLine.length; i++) {
     if(katzDeliLine[i] === name) {
-    return 'Welcome, ' + name + '. You are number ' + (i+1) + ' in line.';
+    return 'Welcome, ' + name + '. You are number ' + i + ' in line.';
     }
   }
 }
